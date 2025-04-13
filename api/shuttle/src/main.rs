c5edae820dce8ad244d1a4302fb1eec35701098d
DevOps Team <147593490+DevOps-2020Insight@users.noreply.github.com>
2025-04-13 13:15:12 +0200

    add Shuttle.toml file

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -5,6 +5,62 @@ use shuttle_actix_web::ShuttleActixWeb;
 async fn hello_world() -> &'static str {
     "Hello World!"
 }
+#[get("/Login")]
+async fn login() -> &'static str {
+    "Login"
+}
+#[get("/Register")]
+async fn register() -> &'static str {
+    "Register"
+}
+#[get("/Movie")]
+async fn movie() -> &'static str {
+    "Movie"
+}
+#[get("/Movie/{id}")]
+async fn movie_id() -> &'static str {
+    "Movie ID"
+}
+#[get("Movie/{id}/Review")]
+async fn movie_review() -> &'static str {
+    "Movie Review"
+}
+#[get("Movie/{id}/Review/{review_id}")]
+async fn movie_review_id() -> &'static str {
+    "Movie Review ID"
+}
+#[get("Movie/{id}/Review/{review_id}/Comment")]
+async fn movie_review_comment() -> &'static str {
+    "Movie Review Comment"
+}
+#[get("Movie/{id}/Review/{review_id}/Comment/{comment_id}")]
+async fn movie_review_comment_id() -> &'static str {
+    "Movie Review Comment ID"
+}
+#[get("Movie/{id}/Review/{review_id}/Comment/{comment_id}/Reply")]
+async fn movie_review_comment_reply() -> &'static str {
+    "Movie Review Comment Reply"
+}
+#[get("Movie/{id}/Review/{review_id}/Comment/{comment_id}/Reply/{reply_id}")]
+async fn movie_review_comment_reply_id() -> &'static str {
+    "Movie Review Comment Reply ID"
+}
+#[get("Movie/{id}/Review/{review_id}/Comment/{comment_id}/Reply/{reply_id}/Like")]
+async fn movie_review_comment_reply_like() -> &'static str {
+    "Movie Review Comment Reply Like"
+}
+#[get("Movie/{id}/Review/{review_id}/Comment/{comment_id}/Reply/{reply_id}/Like/{like_id}")]
+async fn movie_review_comment_reply_like_id() -> &'static str {
+    "Movie Review Comment Reply Like ID"
+}
+#[get("Movie/{id}/Review/{review_id}/Comment/{comment_id}/Reply/{reply_id}/Like/{like_id}/Dislike")]
+async fn movie_review_comment_reply_like_dislike() -> &'static str {
+    "Movie Review Comment Reply Like Dislike"
+}
+#[get("Movie/{id}/Review/{review_id}/Comment/{comment_id}/Reply/{reply_id}/Like/{like_id}/Dislike/{dislike_id}")]
+async fn movie_review_comment_reply_like_dislike_id() -> &'static str {
+    "Movie Review Comment Reply Like Dislike ID"
+}
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
