ff8403dc761178bcb5846f835b50d9a7cd54e397
DevOps Team <147593490+DevOps-2020Insight@users.noreply.github.com>
2025-04-19 15:42:57 +0200

    add database connection

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -64,14 +64,12 @@ async fn movie_review_comment_reply_like_dislike_id() -> &'static str {
     "Movie Review Comment Reply Like Dislike ID"
 }
 
+
+
 #[shuttle_runtime::main]
 async fn actix_web(
     #[shuttle_shared_db::Postgres] pool: sqlx::PgPool,
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
-        
-    pool.execute(include_str!("../../db/schema.sql"))
-        .await
-        .map_err(CustomError::new)?;
 
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_world)
@@ -93,6 +91,9 @@ async fn actix_web(
 
     Ok(config.into())
 }
+
+
+
 
 
 
